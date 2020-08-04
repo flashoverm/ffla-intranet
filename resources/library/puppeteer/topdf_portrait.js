d43741ff584b575a0e410109382102128542153e
flashoverm <markus@thral.de>
2020-08-04 23:12:03 +0200

    V2.0.1 PDF-Creation-Bugfix

---
+++ resources/library/puppeteer/topdf_portrait.js
@@ -0,0 +1,47 @@
+const puppeteer = require('puppeteer');
+
+(async() => {
+
+	try {
+		const browser = await puppeteer.launch({
+			headless: true,
+			args: [
+				'--no-sandbox'
+			]
+		});
+		const page = await browser.newPage();
+		process.on("unhandledRejection", (reason, p) => {
+			console.error("Unhandled Rejection at: Promise", p, "reason:", reason);
+			browser.close();
+		});
+		await page.goto(process.argv[2], {
+			timeout: 12000,
+			waitUntil: 'networkidle2'
+		});
+
+		
+		page.addStyleTag({
+			'content': '@page {size: auto}'
+		});
+		
+		await page.pdf({
+		    margin: {
+		        top: "1cm",
+		        right: "1cm",
+		        bottom: "1cm",
+		        left: "1cm"
+		    },
+			path: process.argv[3], 
+			format: 'A4',
+			landscape: false
+		});
+		
+		await browser.close();
+		
+		console.log("Done");
+	} catch (e) {
+		console.log(e);
+		browser.close();
+	}
+
+})();
