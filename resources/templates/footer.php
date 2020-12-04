516ea84f5bb062f127b4f4eccc84e855e5a2ac38
flashoverm <markus@thral.de>
2020-12-04 23:40:03 +0100

    Mail address update and "how-to-get-access" note

--- resources/templates/footer.php
+++ resources/templates/footer.php
@@ -1,6 +1,6 @@
 	<footer id="footer" class="footer bg-dark">
 		<div class="container text-light">
-			<a class='text-light' href="<?=$config["urls"]["intranet_home"] ?>/impressum">Impressum</a> | Bei Anmerkungen oder Problemen: Email an <a class='text-light' href="mailto:guardian@thral.de?subject=Fehlerbericht%20Intranet" >guardian@thral.de</a>
+			<a class='text-light' href="<?=$config["urls"]["intranet_home"] ?>/impressum">Impressum</a> | Bei Anmerkungen oder Problemen: Email an <a class='text-light' href="mailto:intranet.feuerwehr-landshut@thral.de?subject=Fehlerbericht%20Intranet" >intranet.feuerwehr-landshut@thral.de</a>
 		</div>
 	</footer>
 </body>
