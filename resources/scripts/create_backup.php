4d2387921bd3736bd17d3368756e6d3d4b9a33fb
flashoverm <markus@thral.de>
2021-01-23 00:35:03 +0100

    [WIP] db and data backup to pcloud

--- resources/scripts/create_backup.php
+++ resources/scripts/create_backup.php
@@ -7,6 +7,7 @@ $db_config = $config['db'];
 $today = date('Y-m-d', time());
 
 $db_file = $config['paths']['backup'] . "db_dumps/" . $db_config['dbname'] . "_" . $today;
+$data_file = $config['paths']['backup'] . "db_dumps/data_" . $today . ".zip";
 
 $cmd = "mysqldump " 
 		. " --user=" . $db_config['username']
@@ -15,10 +16,12 @@ $cmd = "mysqldump "
 		. " " . $db_config['dbname']
 		. " > " . $db_file . ".sql";
 
+echo "Creating mysql dump";
 exec($cmd);
 
 $cmd = "zip " . $db_file . ".zip " . $db_file . ".sql";
 
+echo "Zipping mysql dump";
 exec($cmd);
 
 $backup_data = array(
@@ -29,6 +32,18 @@ $backup_data = array(
 		$config['paths']['confirmations'],
 );
 
+$cmd = "zip -R" . $data_file . " ";
+foreach($backup_data as $path){
+	$cmd .= $path . " ";
+}
+
+echo "Zipping data";
+exec($cmd);
 
 $pcloudController = new PCloudController();
-$pcloudController->uploadBackup($filepath);
+
+echo "Uploading dump file (zip)";
+$pcloudController->uploadBackup($db_file . ".zip");
+
+echo "Uploading data file (zip)";
+$pcloudController->uploadBackup($data_file);
