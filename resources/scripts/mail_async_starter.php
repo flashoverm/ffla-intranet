d3b97259e43851ac0395f4162f2d1d51b296106c
flashoverm <markus@thral.de>
2024-08-11 18:50:26 +0200

    Fixed mail async starter script

--- resources/scripts/mail_async_starter.php
+++ resources/scripts/mail_async_starter.php
@@ -2,11 +2,19 @@
 
 require_once realpath ( dirname ( __FILE__ ) . "/../config/config.php" );
 
+echo "\n";
+
 echo trim(shell_exec('ps -ef | grep -v grep | grep mail_async_process.php'));
+
+echo "\n";
 
 if(trim(shell_exec('ps -ef | grep -v grep | grep mail_async_process.php')) == '') {
     
-	echo "mail_async_process not running - starting";
+	echo "mail_async_process not running - starting\n";
 	
-	exec('nohup ' . $config["paths"]["php"] . ' mail_async_process.php >> /dev/null &');
+	$command = 'nohup ' . $config["paths"]["php"] . ' ' . dirname ( __FILE__ ) . '/mail_async_process.php >> /dev/null &';
+	
+	echo "\nCommand: " . $command  . "\n";
+	
+	exec($command);
 }
