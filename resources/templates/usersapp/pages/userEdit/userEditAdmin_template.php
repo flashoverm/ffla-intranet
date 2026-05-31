8fc191113417880ba216c25ea0805aa93fa5fd12
Markus Thral <markus@thral.de>
2026-05-31 20:37:51 +0200

    Fixed displaying default privileges

--- resources/templates/usersapp/pages/userEdit/userEditAdmin_template.php
+++ resources/templates/usersapp/pages/userEdit/userEditAdmin_template.php
@@ -20,7 +20,7 @@
 							  if( 	(isset($user) && $user->hasPrivilegeByName($row->getPrivilege()))
 							  		|| isset($_POST['priv_' . $row->getUuid() ])
 							  		|| (! isset($user) && $_SERVER['REQUEST_METHOD'] === 'GET' && $row->getIsDefault() )
-							        || ( ! $error && $row->getIsDefault())
+							        || ( ! $error && $row->getIsDefault() && !isset($user))
 							  ){
 							  	echo "checked";
 							  }
