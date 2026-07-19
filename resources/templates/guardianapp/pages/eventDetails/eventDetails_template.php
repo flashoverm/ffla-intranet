12443949b964fa21cb34da496c6fc325fd6bbbf7
Markus Thral <markus@thral.de>
2026-07-19 20:36:05 +0200

    Fixed publish error

--- resources/templates/guardianapp/pages/eventDetails/eventDetails_template.php
+++ resources/templates/guardianapp/pages/eventDetails/eventDetails_template.php
@@ -79,61 +79,59 @@ if ($isCreator) {
 	<?php
 	if($currentUser){
 	?>
-	    <form onsubmit="showLoader()" action='<?= $config["urls"]["guardianapp_home"] . "/events/view/" . $event->getUuid() ?>' method='post'>
-			<a href='<?= $config["urls"]["guardianapp_home"] . "/events/overview" ?>' class='btn btn-outline-primary'>Zurück</a>
-			<div class='float-right'>
-			<?php
+		<a href='<?= $config["urls"]["guardianapp_home"] . "/events/overview" ?>' class='btn btn-outline-primary'>Zurück</a>
+		<div class='float-right'>
+		<?php
+		
+		    if( ! $event->getPublished()){
+		    	if( $guardianUserController->isUserAllowedToEditEvent($currentUser, $event->getUuid()) and $relevant) {?>
+	                <span class='d-inline-block' data-toggle='tooltip' title='Andere Züge über Wache informieren'>
+				  		<button type='button' class='btn btn-primary' data-toggle='modal' data-target='#confirmPublish<?= $event->getUuid() ?>'>Veröffentlichen</button>
+	                </span>
+					<?php 
+					createDialog('confirmPublish' . $event->getUuid(), "Wache veröffentlichen <br>(E-Mail an alle möglichen Wachteilnehmer)?", 'publish');
+					?>
+	            <?php 
+		        } else {
+		            echo "<button type='button' class='btn btn-outline-primary mr-1' disabled='disabled' >Wache ist nicht öffentlich</button>";
+		        }
+			} else {
+			        echo "<button type='button' class='btn btn-outline-primary mr-1' disabled='disabled' >Wache ist öffentlich</button>";
+		    }
 			
-			    if( ! $event->getPublished()){
-			    	if( $guardianUserController->isUserAllowedToEditEvent($currentUser, $event->getUuid()) and $relevant) {?>
-		                <span class='d-inline-block' data-toggle='tooltip' title='Andere Züge über Wache informieren'>
-					  		<button type='button' class='btn btn-primary' data-toggle='modal' data-target='#confirmPublish<?= $event->getUuid() ?>'>Veröffentlichen</button>
-		                </span>
-						<?php 
-						createDialog('confirmPublish' . $event->getUuid(), "Wache veröffentlichen <br>(E-Mail an alle möglichen Wachteilnehmer)?", 'publish');
-						?>
-		            <?php 
-			        } else {
-			            echo "<button type='button' class='btn btn-outline-primary mr-1' disabled='disabled' >Wache ist nicht öffentlich</button>";
-			        }
-				} else {
-				        echo "<button type='button' class='btn btn-outline-primary mr-1' disabled='disabled' >Wache ist öffentlich</button>";
-			    }
-				
-			    if( ! $event->isCanceled() && $relevant && $guardianUserController->isUserAllowedToEditEvent($currentUser, $event->getUuid()) ){
-				?>
-				    <button type='button' class='btn btn-primary' data-toggle='modal' data-target='#confirmCancel'>Absagen</button>
-	    			<div class='modal' id='confirmCancel'>
-        				<div class='modal-dialog'>
-        					<div class='modal-content'>
-        						<form method="post" action="">
-        							<div class='modal-header'>
-        								<h4 class='modal-title'>Wache absagen?</h4>
-        								<button type='button' class='close' data-dismiss='modal'>&times;</button>
-        							</div>
-        							<div class='modal-body'>
-        								<div class="form-group">
-        									<input type="text" required="required" placeholder="Grund eingeben" class="form-control" 
-        									name="reason" id="reason" >
-        								</div>
-        								<div><small>Absage kann nicht rückgängig gemacht werden!</small></div>
-        							</div>
-        							<div class='modal-footer'>
-        								<input type='submit' name="request" value='Ja' class='btn btn-primary'/>
-        								<button type='button' class='btn btn-outline-primary' data-dismiss='modal'>Abbrechen</button>
-        							</div>
-        						</form>
-        					</div>
-        				</div>
-        			</div>
-				<?php
-				}
-				
-				if( ! $event->isCanceled() && $guardianUserController->isUserAllowedToEditEvent($currentUser, $event->getUuid()) ) { ?>
-					<a class='btn btn-primary' href='<?= $config["urls"]["guardianapp_home"] ?>/events/edit/<?= $event->getUuid() ?>'>Bearbeiten</a>
-				<?php } ?>
-	    	</div>
-	  	</form>
+		    if( ! $event->isCanceled() && $relevant && $guardianUserController->isUserAllowedToEditEvent($currentUser, $event->getUuid()) ){
+			?>
+			    <button type='button' class='btn btn-primary' data-toggle='modal' data-target='#confirmCancel'>Absagen</button>
+    			<div class='modal' id='confirmCancel'>
+    				<div class='modal-dialog'>
+    					<div class='modal-content'>
+    						<form method="post" action="">
+    							<div class='modal-header'>
+    								<h4 class='modal-title'>Wache absagen?</h4>
+    								<button type='button' class='close' data-dismiss='modal'>&times;</button>
+    							</div>
+    							<div class='modal-body'>
+    								<div class="form-group">
+    									<input type="text" required="required" placeholder="Grund eingeben" class="form-control" 
+    									name="reason" id="reason" >
+    								</div>
+    								<div><small>Absage kann nicht rückgängig gemacht werden!</small></div>
+    							</div>
+    							<div class='modal-footer'>
+    								<input type='submit' name="request" value='Ja' class='btn btn-primary'/>
+    								<button type='button' class='btn btn-outline-primary' data-dismiss='modal'>Abbrechen</button>
+    							</div>
+    						</form>
+    					</div>
+    				</div>
+    			</div>
+			<?php
+			}
+			
+			if( ! $event->isCanceled() && $guardianUserController->isUserAllowedToEditEvent($currentUser, $event->getUuid()) ) { ?>
+				<a class='btn btn-primary' href='<?= $config["urls"]["guardianapp_home"] ?>/events/edit/<?= $event->getUuid() ?>'>Bearbeiten</a>
+			<?php } ?>
+    	</div>
 	<?php
 	}
 	?>
